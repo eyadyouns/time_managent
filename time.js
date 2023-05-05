0bb51fc31a87f7fb71db466c53385f2c2b17b544
eyadyouns <132685123+eyadyouns@users.noreply.github.com>
2023-05-05 17:50:20 +0300

    Add files via upload

---
+++ time.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".show").click(function(){
+    $(".links").fadeIn(1000 );
+    $(".show").fadeOut(500 );
+    $(".hide").fadeIn(500 );
+    });
+    });
+
+
+$(document).ready(function(){
+    $(".hide").click(function(){
+    $(".links").fadeOut(1000 );
+    $(".show").fadeIn(500 );
+    $(".hide").fadeOut(500 );
+    });
+    });
+    
